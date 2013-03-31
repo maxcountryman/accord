78bda7d0a057b611eceb4a36e340b408499899a6
Max Countryman <max.countryman@gmail.com>
2013-03-30 17:17:59 -0700

    adding additional util functions for forthcoming oauth1

--- src/accord/util.clj
+++ src/accord/util.clj
@@ -1,19 +1,36 @@
 (ns accord.util
-  (:require [clj-http.util :refer [url-encode]]))
+  (:require [clj-http.util :refer [url-encode]]
+            [clojure.string :refer [join]]))
 
 
 (def entity-methods [:post :put :patch])
 
 
-(defn encode-params
+(defn a->str
+  [a]
+  (if (instance? clojure.lang.Named a)
+    (name a)
+    (str a)))
+
+
+(defn rfc-3986-url-encode
+  [unencoded]
+  (-> (url-encode unencoded)
+      (.replace "+" "%20")
+      (.replace "*" "%2A")
+      (.replace "%7E" "~")))
+
+
+(defn url-form-encode
   [params]
-  (let [encoded (for [[k v] params]
-                  (str (url-encode (name k)) "=" (url-encode v)))]
-    (apply str (interpose "&" encoded))))
+  (join "&" (map (fn [[k v]]
+                   (str (rfc-3986-url-encode (a->str k))
+                        "="
+                        (rfc-3986-url-encode (a->str v)))) params)))
 
 
 (defn parse-qs
-  [uri] 
+  [uri]
   (into {} (for [[_ k v] (re-seq #"([^&=]+)=([^&]+)" uri)]
     [(keyword k) v])))
 
